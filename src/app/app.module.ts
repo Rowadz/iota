d27b5049bf1ea6d03d4b1705a669293122328d73
Rowad <rowadwo@gmail.com>
2019-10-14 01:59:37 +0300

    Basic Structure
    Added NES.CSS and bootstrap-4-grid

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OptionsComponent } from './components/options/options.component';
+import { ImageContainerComponent } from './components/image-container/image-container.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OptionsComponent,
+    ImageContainerComponent,
+    ToolbarComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/components/image-container/image-container.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'iota-image-container',
+  templateUrl: './image-container.component.html',
+  styleUrls: ['./image-container.component.scss']
+})
+export class ImageContainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
